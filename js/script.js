63751facd1e0f9f8f65208d45cf3f90c266fbcdc
Lacki Kritsimas <l.kritsimas@gmail.com>
2019-11-05 20:14:36 +0100

    Add dynamic rendering

--- js/script.js
+++ js/script.js
@@ -1,8 +1,67 @@
+// Available languages
 let languages = ['en', 'sv'];
-let dialogButtons = document.querySelectorAll('.dialog__footer button');
 
+var selectedWord; // Current word (randomly selected)
+var guessedLetters; // Letters that have been guessed so far
+
+var guesses; // Attempts before game over
+var guessTime; // Countdown timer
+
+var isRunning; // Is game running?
+
+// Audio
+var sounds = {
+  dialogButton: 'dialog-button.mp3',
+  alphabetButton: 'alphabet-button.mp3',
+  correct: 'correct.mp3',
+  incorrect: 'incorrect.mp3',
+  tick: 'tick.wav',
+  tock: 'tock.wav'
+};
+
+// Elements
+let elDialog = document.querySelector('.dialog');
+let dialogButtons = document.querySelectorAll('.dialog__footer button');
+let elButtonContainer = document.querySelector('#alphabet');
+
+// Get language (default: English)
 getLanguage();
 
+function guessClick() {
+  if (guesses === 0) return;
+
+  var letter = this.value;
+
+  // Was the guess right or wrong?
+  if (selectedWord.indexOf(letter) === -1) {
+    guesses--;
+  } else {
+    this.classList.add('selected');
+  }
+
+  guessedLetters.push(letter);
+  // renderLetters();
+  // renderCanvas();
+  playSound(sounds.alphabetButton);
+  this.disabled = true;
+}
+
+function start() {
+  playSound(sounds.dialogButton);
+
+  // Get a random word from word list
+  selectedWord = _language.wordList[Math.floor(Math.random() * _language.wordList.length)];
+
+  guesses = 6;
+  guessedLetters = [];
+
+  startTime = +new Date();
+  isRunning = true;
+
+  elDialog.style.display = 'none';
+}
+
+// Translation
 (function translate() {
   let elLanguage = document.querySelector('#language');
 
@@ -22,6 +81,7 @@ getLanguage();
     // Check if current language is in localstorage
     if (language === localStorage.getItem('language')) {
       button.classList.add('selected');
+      button.disabled = true;
     }
 
     button.addEventListener('click', function() {
@@ -33,7 +93,14 @@ getLanguage();
     elLanguage.querySelector('ul').appendChild(button);
   });
 
-  dialogButtons.forEach(button => {
-    button.textContent = _language[button.value];
-  });
+  // Render dialog
+  renderDialog(_language.newGame, null, [
+    {
+      text: _language.startGame,
+      click: start
+    }
+  ]);
+
+  // Render alphabet buttons
+  renderButtons(elButtonContainer, _language.alphabet, guessClick);
 })();
--- js/utils.js
+++ js/utils.js
@@ -23,3 +23,59 @@ function getLanguage() {
 function setLanguage(language) {
   localStorage.setItem('language', language);
 }
+
+// Play a sound file
+function playSound(file, volume = 0.25) {
+  var audio = new Audio('sound/' + file);
+  audio.volume = volume;
+  audio.play();
+}
+
+// Render alphabet buttons
+function renderButtons(container, alphabet, click) {
+  container.innerHTML = '';
+
+  for (let i = 0; i < alphabet.length; i++) {
+    let elItem = document.createElement('li');
+    let elButton = document.createElement('button');
+    elButton.classList.add('btn');
+    elButton.classList.add('btn--letter');
+    elButton.value = alphabet[i];
+    elButton.textContent = alphabet[i];
+    elButton.addEventListener('click', click);
+
+    elItem.appendChild(elButton);
+    container.appendChild(elItem);
+  }
+}
+
+// Render dialog
+function renderDialog(title, message = null, buttons, className = null) {
+  let container = document.querySelector('.dialog__content');
+  let heading = container.querySelector('h2');
+
+  let footer = container.querySelector('.dialog__footer');
+  footer.innerHTML = '';
+
+  // Loop through buttons and set up listener
+  for (let button of buttons) {
+    let elButton = document.createElement('button');
+    elButton.classList.add('btn');
+    elButton.textContent = button.text;
+    elButton.addEventListener('click', button.click);
+
+    footer.appendChild(elButton);
+  }
+
+  heading.textContent = title;
+  container.prepend(heading);
+
+  if (className) container.className = className;
+
+  // Render message if set
+  if (message) {
+    let elMessage = document.createElement('p');
+    elMessage.textContent = message;
+    container.appendChild(elMessage);
+  }
+}
